1b8b0540083f2470fc7db19721fa10b882b1a812
Сергей Князьков <knyazkovsv@rushydro.ru>
2024-04-24 01:11:49 +0400

    Выполнен шаг 3

---
+++ gendiff/gendiff.py
@@ -0,0 +1,33 @@
+import json
+
+
+def generate_diff(file_path1, file_path2):
+
+    json1 = json.load(open(file_path1))
+    json2 = json.load(open(file_path2))
+
+    items = []
+    for key, value1 in json1.items():
+        if key in json2:
+            value2 = json2[key]
+            if value1 == value2:
+                items.append((key, 0, value1))
+            else:
+                items.append((key, 1, value1))
+                items.append((key, 2, value2))
+            del json2[key]
+        else:
+            items.append((key, 1, value1))
+
+    for key, value2 in json2.items():
+        items.append((key, 2, value2))
+
+    items.sort()
+
+    replaces = {1: '-', 2: '+', 0: ' '}
+    lines = [
+        f'  {replaces[place]} {key}: {value}'
+        for key, place, value
+        in items
+    ]
+    return '{\n' + '\n'.join(lines) + '\n}\n'
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,6 +2,8 @@
 
 import argparse
 
+from gendiff.gendiff import generate_diff
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -11,7 +13,9 @@ def main():
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output')
-    parser.parse_args()
+    args = parser.parse_args()
+    diff = generate_diff(args.first_file, args.second_file)
+    print(diff)
 
 
 if __name__ == '__main__':

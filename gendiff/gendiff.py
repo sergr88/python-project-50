f2247b726da9bc1fa87a3460c56314a3ae97d3ee
Сергей Князьков <knyazkovsv@rushydro.ru>
2024-04-30 19:17:34 +0400

    Проведен рефакторинг

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,38 +1,29 @@
 import json
+from operator import itemgetter
 
 
-def replace_value(value):
-    if isinstance(value, bool):
-        return str(value).lower()
-    return value
+def replaced_bool(value):
+    return str(value).lower() if isinstance(value, bool) else value
 
 
 def generate_diff(file_path1, file_path2):
     json1 = json.load(open(file_path1))
     json2 = json.load(open(file_path2))
 
-    items = []
-    for key, value1 in json1.items():
-        if key in json2:
-            value2 = json2[key]
-            if value1 == value2:
-                items.append((key, 0, value1))
-            else:
-                items.append((key, 1, value1))
-                items.append((key, 2, value2))
-            del json2[key]
-        else:
-            items.append((key, 1, value1))
-
-    for key, value2 in json2.items():
-        items.append((key, 2, value2))
-
-    items.sort()
-
-    replaces = {1: '-', 2: '+', 0: ' '}
-    lines = [
-        f'  {replaces[place]} {key}: {replace_value(value)}'
-        for key, place, value
-        in items
+    diff1 = [
+        (' ' if value == json2.get(key) else '-', key, value)
+        for key, value in json1.items()
     ]
-    return '{\n' + '\n'.join(lines) + '\n}\n'
+    diff2 = [
+        ('+', key, value)
+        for key, value in json2.items()
+        if value != json1.get(key)
+    ]
+    difference = sorted(diff1 + diff2, key=itemgetter(1))
+
+    lines = [
+        f'  {prefix} {key}: {replaced_bool(value)}'
+        for prefix, key, value
+        in difference
+    ]
+    return '\n'.join(['{', *lines, '}'])

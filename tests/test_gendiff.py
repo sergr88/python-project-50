d6553dd464e1ceb96c8298800f90d08a918670fb
Сергей Князьков <knyazkovsv@rushydro.ru>
2024-05-01 00:08:20 +0400

    Проведен рефакторинг тестов

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,9 +1,13 @@
+import os.path
+
 from gendiff.gendiff import generate_diff
 
+BASE_PATH = os.path.join('tests', 'fixtures')
 
-def test_main():
-    with open('./tests/fixtures/result.txt', 'r') as input_file:
+
+def test_plain_json():
+    with open(os.path.join(BASE_PATH, 'result.txt')) as input_file:
         assert generate_diff(
-            './tests/fixtures/input1.json',
-            './tests/fixtures/input2.json'
+            os.path.join(BASE_PATH, 'flat_input1.json'),
+            os.path.join(BASE_PATH, 'flat_input2.json')
         ) == input_file.read()

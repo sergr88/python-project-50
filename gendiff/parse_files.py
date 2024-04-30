b7b32c043907dc6c7d22584dbbf008f4eaaed8e4
Сергей Князьков <knyazkovsv@rushydro.ru>
2024-05-01 01:20:55 +0400

    Добавлен функционал сравнения плоских YAML-файлов

--- gendiff/parse_files.py
+++ gendiff/parse_files.py
@@ -1,4 +1,18 @@
 import json
+import os.path
+
+import yaml
+
+
+def parse_file(file_path):
+    _, extension = os.path.splitext(file_path)
+    match extension.lower():
+        case '.json':
+            return json.load(open(file_path))
+        case '.yaml' | '.yml':
+            return yaml.load(open(file_path), Loader=yaml.Loader)
+        case _:
+            return ''
 
 
 def parse_files(file_path1, file_path2):

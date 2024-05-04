40f1b3f8137b1e4c2a411823f026bef5f7839e1c
Сергей Князьков <knyazkovsv@rushydro.ru>
2024-05-04 14:42:46 +0400

    Добавлены тесты сравнения файлов со вложенностью

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -19,3 +19,19 @@ def test_plain_yaml():
             os.path.join(BASE_PATH, 'flat_input1.yaml'),
             os.path.join(BASE_PATH, 'flat_input2.yaml')
         ) == input_file.read()
+
+
+def test_nested_json():
+    with open(os.path.join(BASE_PATH, 'nested_result.txt')) as input_file:
+        assert generate_diff(
+            os.path.join(BASE_PATH, 'nested_input1.json'),
+            os.path.join(BASE_PATH, 'nested_input2.json')
+        ) == input_file.read()
+
+
+def test_nested_yaml():
+    with open(os.path.join(BASE_PATH, 'nested_result.txt')) as input_file:
+        assert generate_diff(
+            os.path.join(BASE_PATH, 'nested_input1.yaml'),
+            os.path.join(BASE_PATH, 'nested_input2.yaml')
+        ) == input_file.read()

a742b430dd5cb3730d6fe3836b1e56b86421d9ae
Сергей Князьков <knyazkovsv@rushydro.ru>
2024-05-05 14:33:44 +0400

    Проведен рефакторинг тестов

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,37 +1,37 @@
 import os.path
 
-from gendiff.gendiff import generate_diff
+from gendiff import generate_diff
 
 BASE_PATH = os.path.join('tests', 'fixtures')
 
 
 def test_plain_json():
     with open(os.path.join(BASE_PATH, 'flat_result.txt')) as input_file:
-        assert generate_diff(
+        difference = generate_diff(
             os.path.join(BASE_PATH, 'flat_input1.json'),
-            os.path.join(BASE_PATH, 'flat_input2.json')
-        ) == input_file.read()
+            os.path.join(BASE_PATH, 'flat_input2.json'))
+        assert difference == input_file.read()
 
 
 def test_plain_yaml():
     with open(os.path.join(BASE_PATH, 'flat_result.txt')) as input_file:
-        assert generate_diff(
+        difference = generate_diff(
             os.path.join(BASE_PATH, 'flat_input1.yaml'),
-            os.path.join(BASE_PATH, 'flat_input2.yaml')
-        ) == input_file.read()
+            os.path.join(BASE_PATH, 'flat_input2.yaml'))
+        assert difference == input_file.read()
 
 
 def test_nested_json():
     with open(os.path.join(BASE_PATH, 'nested_result.txt')) as input_file:
-        assert generate_diff(
+        difference = generate_diff(
             os.path.join(BASE_PATH, 'nested_input1.json'),
-            os.path.join(BASE_PATH, 'nested_input2.json')
-        ) == input_file.read()
+            os.path.join(BASE_PATH, 'nested_input2.json'))
+        assert difference == input_file.read()
 
 
 def test_nested_yaml():
     with open(os.path.join(BASE_PATH, 'nested_result.txt')) as input_file:
-        assert generate_diff(
+        difference = generate_diff(
             os.path.join(BASE_PATH, 'nested_input1.yaml'),
-            os.path.join(BASE_PATH, 'nested_input2.yaml')
-        ) == input_file.read()
+            os.path.join(BASE_PATH, 'nested_input2.yaml'))
+        assert difference == input_file.read()

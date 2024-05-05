01685ff6b7100b4c186a03c4924cde4f77d50038
Сергей Князьков <knyazkovsv@rushydro.ru>
2024-05-05 12:25:18 +0400

    Добавлено вычисление разницы файлов со вложенностью

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -4,18 +4,33 @@ from gendiff.formatters import format_stylishly
 from gendiff.parsers import parse_files
 
 
+def is_dict(value):
+    return isinstance(value, dict)
+
+
+def calculate_difference(properties1, properties2):
+    diff1 = []
+    for key, value1 in properties1.items():
+        value2 = properties2.get(key)
+        if is_dict(value1) and is_dict(value2):
+            node_difference = ' ', key, calculate_difference(value1, value2)
+        elif value1 == value2:
+            node_difference = ' ', key, value1
+        else:
+            node_difference = '-', key, value1
+        diff1.append(node_difference)
+
+    diff2 = []
+    for key, value2 in properties2.items():
+        value1 = properties1.get(key)
+        if value2 == value1 or (is_dict(value2) and is_dict(value1)):
+            continue
+        diff2.append(('+', key, value2))
+
+    return sorted(diff1 + diff2, key=itemgetter(1))
+
+
 def generate_diff(file_path1, file_path2):
     properties1, properties2 = parse_files(file_path1, file_path2)
-
-    diff1 = [
-        (' ' if value == properties2.get(key) else '-', key, value)
-        for key, value in properties1.items()
-    ]
-    diff2 = [
-        ('+', key, value)
-        for key, value in properties2.items()
-        if value != properties1.get(key)
-    ]
-    difference = sorted(diff1 + diff2, key=itemgetter(1))
-
+    difference = calculate_difference(properties1, properties2)
     return format_stylishly(difference)

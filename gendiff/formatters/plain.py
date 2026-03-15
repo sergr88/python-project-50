7f3f915856f6d811668162329c38819a1b7ed1d0
Сергей Князьков <knyazkovsv@rushydro.ru>
2026-03-15 09:08:25 +0400

    Проведен рефакторинг для устранены замечания линтера о цикломатической сложности

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -18,6 +18,30 @@ def format_value(value):
             return value
 
 
+def format_unchanged(value, current_path, parent_path):
+    if isinstance(value, list):
+        return format_node(value, f'{current_path}.')
+    return None
+
+
+def format_removed(nodes, key, value, line_prefix):
+    is_found, value2 = find_value(nodes, '+', key)
+    if is_found:
+        return (
+            f'{line_prefix}'
+            f' updated. From {format_value(value)}'
+            f' to {format_value(value2)}'
+        )
+    return f'{line_prefix} removed'
+
+
+def format_added(nodes, key, value, line_prefix):
+    is_found, _ = find_value(nodes, '-', key)
+    if not is_found:
+        return f'{line_prefix} added with value: {format_value(value)}'
+    return None
+
+
 def format_node(nodes, parent_path):
     lines = []
     for prefix, key, value in nodes:
@@ -25,21 +49,15 @@ def format_node(nodes, parent_path):
         line_prefix = f"Property '{current_path}' was"
         match prefix:
             case ' ':
-                if isinstance(value, list):
-                    lines.append(format_node(value, f'{current_path}.'))
+                result = format_unchanged(value, current_path, parent_path)
             case '-':
-                is_found, value2 = find_value(nodes, '+', key)
-                if is_found:
-                    lines.append(f'{line_prefix}'
-                                 f' updated. From {format_value(value)}'
-                                 f' to {format_value(value2)}')
-                else:
-                    lines.append(f'{line_prefix} removed')
+                result = format_removed(nodes, key, value, line_prefix)
             case '+':
-                is_found, _ = find_value(nodes, '-', key)
-                if not is_found:
-                    lines.append(f'{line_prefix}'
-                                 f' added with value: {format_value(value)}')
+                result = format_added(nodes, key, value, line_prefix)
+            case _:
+                result = None
+        if result is not None:
+            lines.append(result)
     return '\n'.join(lines)
 
 
